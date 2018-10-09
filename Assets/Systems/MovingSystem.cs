35d230ab602f053851ee5402ac5e872084a8117e
tkchouaki <tkchouaki@icloud.com>
2018-10-09 15:15:36 +0200

    Renamed NewMovingSystem to MovingSystem Renamed NewMovable to Movable

--- Assets/Systems/MovingSystem.cs
+++ Assets/Systems/MovingSystem.cs
@@ -2,72 +2,75 @@
 using FYFY;
 
 public class MovingSystem : FSystem {
-    private readonly Family _movables = FamilyManager.getFamily(new AllOfComponents(
-        typeof(Movable), typeof(Rigidbody)
-    ));
-    private readonly Family _pathogenes = FamilyManager.getFamily(new AnyOfTags("pathogene"));
-    private readonly Family _defenses = FamilyManager.getFamily(new AnyOfTags("defense_site"));
+    private Family family = FamilyManager.getFamily(new AllOfComponents(typeof(Movable)));
 
-	// Use this to update member variables when system pause. 
-	// Advice: avoid to update your families inside this function.
-	protected override void onPause(int currentFrame) {
+    protected override void onPause(int currentFrame) {
 	}
 
 	// Use this to update member variables when system resume.
 	// Advice: avoid to update your families inside this function.
 	protected override void onResume(int currentFrame){
+
 	}
 
 	// Use to process your families.
+    // Loop throught the game objects inside the family
+    // For each moving object, we direct it to its target (if it has one)
+    // We compute the forces applied by the nearby objects that are in conflict (we use the layer mask for this one)
 	protected override void onProcess(int familiesUpdateCount) {
-        foreach (GameObject go in _movables)
+        // Loop throught the game objects inside the family
+        foreach (GameObject objectA in family)
         {
-            Movable movable = go.GetComponent<Movable>();
-            Rigidbody rbody = go.GetComponent<Rigidbody>();
-
-            Vector3 velocity = new Vector3();
-            foreach (GameObject pathogene in _pathogenes)
+            
+            Movable movableA = objectA.GetComponent<Movable>();
+            if (movableA.is_moving)
             {
-                if (!pathogene.Equals(go))
+                // For each moving object, we direct it to its target (if it has one)
+                Vector3 newVelocity = new Vector3();
+                if (movableA.target != null)
                 {
-                    Vector3 r = go.transform.position - pathogene.transform.position;
-                    Vector3 u = new Vector3(r.x, r.y, r.z);
-                    u.Normalize();
-
-                    float a = .6f;
-                    float b = .1f;
-                    float n = 2;
-                    float m = 1f;
-                    float d = r.magnitude;
-
-                    float U = a / Mathf.Pow(d, n) - b / Mathf.Pow(d, m);
-
-                    velocity += U * u;
+                    Vector3 targetPosition = movableA.target.transform.position;
+                    Vector3 toTarget = targetPosition - objectA.transform.position;
+                    if(toTarget.magnitude > movableA.safetyDistance)
+                    {
+                        newVelocity += toTarget.normalized;
+                    }
                 }
+                // We compute the forces applied by the nearby objects that are in conflict (we use the layer mask for this one)
+                foreach (GameObject objectB in family)
+                {
+                    //We make sure to avoid the same object
+                    if (objectA.Equals(objectB)) continue;
+
+                    Movable movableB = objectB.GetComponent<Movable>();
+
+                    //We apply an AND operator to check if the objects are on a same layer
+                    int and = movableA.layer_mask & movableB.layer_mask;
+
+                    Vector3 r = (objectA.transform.position - objectB.transform.position);
+
+                    //We retrieve the distance between them
+                    float distance = r.magnitude;
+
+                    // we should apply a force if the two objects occupy a same layer & the distance between them is inside the range
+                    if (and > 0 && distance < movableA.range)
+                    {
+                        Vector3 u = r.normalized;
+                        float a = 2.0f;
+                        float b = 0.0f;
+                        float n = 2f;
+                        float m = 2f;
+
+                        //We compute the force to apply
+                        float U = a / Mathf.Pow(distance, n) - b / Mathf.Pow(distance, m);
+
+                        newVelocity += u * U;
+                    }
+                }
+                //We normalize the velocity
+                newVelocity.Normalize();
+                objectA.GetComponent<Rigidbody>().velocity = newVelocity * movableA.velocity;
             }
-            foreach (GameObject target in _defenses)
-            {
-                Vector3 r = go.transform.position - target.transform.position;
-                Vector3 u = new Vector3(r.x, r.y, r.z);
-                u.Normalize();
-
-                float a = .4f;
-                float b = .4f;
-                float n = 2f;
-                float m = 1f;
-                float d = r.magnitude;
-
-                float U = a / Mathf.Pow(d, n) - b / Mathf.Pow(d, m);
-
-                velocity += U * u;
-            }
-            velocity.Normalize();
-            velocity = new Vector3(
-                Mathf.Round(velocity.x),
-                Mathf.Round(velocity.y),
-                Mathf.Round(velocity.z)
-            );
-            rbody.velocity = movable.velocity * velocity;
         }
-	}
+    }
 }
