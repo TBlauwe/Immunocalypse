d51972f34f9941bafa16e514e3065171a472a4de
Pierre DUBAILLAY <pierredubaillay@outlook.fr>
2018-10-09 18:14:36 +0200

    Added Eating related stuff

---
+++ Assets/Components/Eater.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(WithHealth))]
+public class Eater : MonoBehaviour {
+    public float cooldown;
+    public float nextMeal;
+    public float eatingLimitBeforeDeath;
+    public SphereCollider eatingCollider;
+    public int eatMask;
+}
