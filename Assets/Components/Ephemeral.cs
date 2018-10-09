aae508fa1fd4f2e8bfcb625e650b9f99119b1c61
tkchouaki <tkchouaki@icloud.com>
2018-10-09 15:53:39 +0200

    Added Ephemeral component Added EphemeralSystem

---
+++ Assets/Components/Ephemeral.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(WithHealth))]
+public class Ephemeral : MonoBehaviour {
+    public float lifetime = 60;
+    public bool degressiveHealth = false;
+    [HideInInspector] public float elapsedTime = 0;
+    [HideInInspector] public float startingHealth = 0;
+}
