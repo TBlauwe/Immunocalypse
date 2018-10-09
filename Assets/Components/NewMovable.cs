8c5d6a153e40e45b8346ba4e12ab4ab5fd10e9c7
tkchouaki <tkchouaki@icloud.com>
2018-10-09 14:23:57 +0200

    Added NewMovable component & NewMovingSystem

---
+++ Assets/Components/NewMovable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class NewMovable : MonoBehaviour {
+    public static int LAYER_1 = 1;
+    public static int LAYER_2 = 2;
+    public static int LAYER_3 = 3;
+    public static int LAYER_4 = 4;
+    public static int LAYER_5 = 5;
+    public static int LAYER_6 = 6;
+    public static int LAYER_7 = 7;
+    public static int LAYER_8 = 8;
+
+    public int layer_mask = 0;
+    public float range = 5;
+    public float safetyDistance = 2;
+    public bool is_moving;
+    public float velocity = 2;
+    public Vector3 target;
+}
