f857a5dfdd8c26e6786213f34767c2063d679b2d
tkchouaki <tkchouaki@icloud.com>
2018-10-11 21:14:18 +0200

    Added Damager Component Added Destroyable Component Added Damaging System

---
+++ Assets/Components/Damager.cs
@@ -0,0 +1,8 @@
+﻿using FYFY_plugins.TriggerManager;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Damager : MonoBehaviour {
+    public float attackPoints = 3;
+    public int damagablesLayerMask = 0;
+}
---
+++ Assets/Systems/DamagingSystem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using FYFY;
+using FYFY_plugins.TriggerManager;
+
+public class DamagingSystem : FSystem {
+
+    private Family damagersFamily = FamilyManager.getFamily(new AllOfComponents(typeof(Damager)));
+    private Family destroyablesFamily = FamilyManager.getFamily(new AllOfComponents(typeof(Destroyable)));
+
+
+	// Use this to update member variables when system pause. 
+	// Advice: avoid to update your families inside this function.
+	protected override void onPause(int currentFrame) {
+	}
+
+	// Use this to update member variables when system resume.
+	// Advice: avoid to update your families inside this function.
+	protected override void onResume(int currentFrame){
+
+    }
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+        foreach(GameObject attacker in damagersFamily)
+        {
+            Triggered3D trigger3D = attacker.GetComponent<Triggered3D>();
+            Damager damager = attacker.GetComponent<Damager>();
+            foreach (GameObject target in trigger3D.Targets)
+            {
+                if (target.Equals(attacker)) continue;
+                if (destroyablesFamily.contains(target.GetInstanceID()))
+                {
+                    Destroyable destroyable = target.GetComponent<Destroyable>();
+                    if ((damager.damagablesLayerMask & destroyable.layerMask) > 0)
+                    {
+                        WithHealth health = target.GetComponent<WithHealth>();
+                        if (damager.attackPoints > destroyable.defensePoints)
+                        {
+                            health.health -= damager.attackPoints - destroyable.defensePoints;
+                        }
+                    }
+                }
+            }
+        }
+	}
+}
