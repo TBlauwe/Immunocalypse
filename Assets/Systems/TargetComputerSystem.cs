e1ace38a811f98654f7c8ce6319b647cfccb2f36
tkchouaki <tkchouaki@icloud.com>
2018-10-15 12:32:45 +0200

    Changed TargetComputerSystem

--- Assets/Systems/TargetComputerSystem.cs
+++ Assets/Systems/TargetComputerSystem.cs
@@ -2,31 +2,29 @@
 using FYFY;
 
 public class TargetComputerSystem : FSystem {
-    private readonly Family _pathogenes = FamilyManager.getFamily(new AnyOfTags("pathogene"));
-    private readonly Family _immunos = FamilyManager.getFamily(new AnyOfTags("immuno"));
+    private readonly Family _pathogenes = FamilyManager.getFamily(new AnyOfTags("pathogene"), new AnyOfComponents(typeof(Movable)));
+    private readonly Family _immunos = FamilyManager.getFamily(new AnyOfTags("immuno"), new AnyOfComponents(typeof(Movable)));
     private readonly Family _defense_sites = FamilyManager.getFamily(new AnyOfTags("defense_site"));
 
     private readonly Family _movables = FamilyManager.getFamily(new AllOfComponents(typeof(Movable)));
 
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
-        foreach (GameObject go in _movables)
+        foreach (GameObject gameObject in _pathogenes)
         {
-            Movable movable = go.GetComponent<Movable>();
-            if (movable.target == null)
+            Movable movable = gameObject.GetComponent<Movable>();
             {
-                string tag = go.tag;
-                Debug.Log(tag);
-                switch(tag)
-                {
-                    case "pathogene":
-                        if (_defense_sites.Count > 0) movable.target = _defense_sites.First();
-                        break;
-                    case "immuno":
-                        if (_pathogenes.Count > 0) movable.target = _pathogenes.First();
-                        break;
-                }
+                if (_defense_sites.Count > 0) movable.target = _defense_sites.First();
             }
         }
-	}
+
+        foreach (GameObject gameObject in _immunos)
+        {
+            Movable movable = gameObject.GetComponent<Movable>();
+            if (movable.target == null)
+            {
+                if (_pathogenes.Count > 0) movable.target = _pathogenes.First();
+            }
+        }
+    }
 }
