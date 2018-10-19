851daf146bb2eae6292193dcd71e5784d8215cba
Pierre DUBAILLAY <pierredubaillay@outlook.fr>
2018-10-19 07:46:33 +0200

    A first example of what the game interface could be

---
+++ Assets/Components/UI/CardPanel/Card.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Card : MonoBehaviour {
+    public GameObject entityPrefab;
+    public float energyCost;
+    public bool shouldDisplay;
+}
---
+++ Assets/Systems/DragAndDropSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using FYFY;
+using FYFY_plugins.PointerManager;
+
+public class DragAndDropSystem : FSystem {
+    private readonly Family _cards = FamilyManager.getFamily(
+        new AllOfComponents(typeof(Dragable), typeof(Card))
+    );
+
+	protected override void onProcess(int familiesUpdateCount) {
+        foreach (GameObject go in _cards)
+        {
+            Dragable dragable = go.GetComponent<Dragable>();
+            if (dragable.isDragged)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    go.transform.position = Input.mousePosition;
+                }
+                else
+                {
+                    dragable.isDragged = false;
+                }
+            }
+            else
+            {
+                if (go.GetComponent<PointerOver>() != null)
+                {
+                    GameObject duplicate = Object.Instantiate(go);
+                    GameObjectManager.bind(duplicate);
+
+                    duplicate.GetComponent<Dragable>().isDragged = true;
+                }
+            }
+        }
+	}
+}
