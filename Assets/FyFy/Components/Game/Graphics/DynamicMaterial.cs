dabe701b67ad82aa3e063187dbbc8ca62b9e2898
Tristan <tdb.work@outlook.fr>
2019-01-02 17:51:01 +0100

    Added dynamic material link on health

--- Assets/FyFy/Components/Game/Graphics/DynamicMaterial.cs
+++ Assets/FyFy/Components/Game/Graphics/DynamicMaterial.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(WithHealth))]
 public class DynamicMaterial : MonoBehaviour {
+    public float  percentage=1;     // Used to determine which materials should be changed
     public string materialName;     // Used to determine which materials should be changed
     public Color startingColor;     // Explicit
     public Color endingColor;       // Explicit
