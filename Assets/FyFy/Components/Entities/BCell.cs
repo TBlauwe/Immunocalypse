3f34071de02314ad511fad6be341878669c81775
Pierre DUBAILLAY <pierredubaillay@outlook.fr>
2019-01-07 09:46:03 +0100

    Added BCell FYFY related stuff

---
+++ Assets/FyFy/Components/Entities/BCell.cs
@@ -0,0 +1,11 @@
+﻿using FYFY_plugins.PointerManager;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class BCell : MonoBehaviour {
+    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+    public int freezeMask;
+    public float delay;
+    [HideInInspector] public float cooldown;
+    public float freezeTime;
+}
---
+++ Assets/FyFy/Systems/FreezeSystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class FreezeSystem : FSystem {
+    private readonly Family _frozen = FamilyManager.getFamily(
+        new AllOfComponents(typeof(Frozen)),
+        new NoneOfComponents(typeof(Removed))
+    );
+	
+    // Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+        foreach (GameObject go in _frozen)
+        {
+            Frozen frozen = go.GetComponent<Frozen>();
+            frozen.remainingTime -= Time.deltaTime;
+
+            if (frozen.remainingTime <= 0)
+            {
+                GameObjectManager.removeComponent(frozen);
+            }
+        }
+	}
+}
