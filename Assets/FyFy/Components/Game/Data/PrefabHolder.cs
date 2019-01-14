167a0aecf4db7d5cf18add3b5005f6aa6cbb17e7
Tristan <tdb.work@outlook.fr>
2019-01-14 17:00:06 +0100

    Writings docs

--- Assets/FyFy/Components/Game/Data/PrefabHolder.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class PrefabHolder : MonoBehaviour {
-    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
-    public GameObject SimpleBacteria;
-    public GameObject SimpleVirus;
-}
