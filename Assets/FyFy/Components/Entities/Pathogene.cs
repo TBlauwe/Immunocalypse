5fd9b28ca3ecdb839f500af095a84c4805cf94ef
Pierre DUBAILLAY <pierredubaillay@outlook.fr>
2019-01-17 11:12:53 +0100

    Added Norovirus and related Immuno cell
    Also fixed autoplay BCell particules and Prefab locations

---
+++ Assets/FyFy/Components/Entities/Pathogene.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class Pathogene : MonoBehaviour {
+    public GameObject target;
+}
--- Assets/FyFy/Components/Entities/Virus.cs
+++ Assets/FyFy/Components/Entities/Virus.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Origin))]
-public class Virus : MonoBehaviour {
-    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
-    public GameObject target;
-}
+public class Virus : MonoBehaviour {}
